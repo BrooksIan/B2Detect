89fa492a07223f3fb69640c4af0c4fabdde1d350
Ian Brooks <IBrooks@hortonworks.com>
2019-08-08 13:58:00 -0400

    Update callTFModel.py

--- callTFModel.py
+++ callTFModel.py
@@ -7,13 +7,11 @@ import time
 import json
 import sys
 
-
 imagePath = str(sys.argv[1])
-#output_name = str(sys.argv[2])
-threshold=0.95 #= str(sys.argv[3])
+threshold=0.95
 timeTheashold = 2.5
 
-image = PIL.Image.open(imagePath)  # Change dog.jpg with your image
+image = PIL.Image.open(imagePath
 image_np = numpy.array(image)
 draw = ImageDraw.Draw(image)
 
@@ -21,15 +19,12 @@ draw = ImageDraw.Draw(image)
 payload = {"instances": [image_np.tolist()]}
 start = time.time()
 res = requests.post("http://public-sector-das0.field.hortonworks.com:8501/v1/models/saved_model:predict", json=payload)
-#print(f"Took {time.perf_counter()-start:.2f}s")
 processTime = time.time()-start
-
 
 jsonStr= json.dumps(res.json())
 jsonDict = json.loads(jsonStr)
 
 predScore = jsonDict['predictions'][0]['detection_scores'][0]
-#topDetectionBox = jsonDict['predictions'][0]['detection_boxes'][0]
 
 if((predScore >= threshold) and (timeTheashold >= processTime)):
 	response = {"response":"B2Found","confidence": predScore , "duration": processTime }
