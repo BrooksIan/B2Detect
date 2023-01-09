324fe419072c848ff67ce76cbeeddfd45ac46aee
Ian Brooks <ibrooks@cloudera.com>
2023-01-09 15:50:15 -0500

    Update callDummyModel.py

--- callDummyModel.py
+++ callDummyModel.py
@@ -1,36 +1,12 @@
-#import PIL.Image
-#from PIL import ImageDraw
-#import numpy
-#import requests
-#from pprint import pprint
-#import time
-#import json
-#import sys
+from pprint import pprint
+import json
+import sys
 
-#imagePath = str(sys.argv[1])
-#threshold=0.95
-#timeTheashold = 2.5
+imagePath = str(sys.argv[1])
+threshold=0.95
+timeTheashold = 2.5
 
-#image = PIL.Image.open(imagePath)
-#image_np = numpy.array(image)
-#draw = ImageDraw.Draw(image)
-
-
-#payload = {"instances": [image_np.tolist()]}
-#start = time.time()
-#res = requests.post("http://0.0.0.0:8501/v1/models/saved_model:predict", json=payload)
-#processTime = time.time()-start
-
-#jsonStr= json.dumps(res.json())
-#jsonDict = json.loads(jsonStr)
-
-#predScore = jsonDict['predictions'][0]['detection_scores'][0]
-
-if(true):
-	response = {"response":"B2Found","confidence":"99.999" , "duration":"88.88"}
-
-#else:
-#	response = {"response":"B2NotFound","confidence":predScore,"duration":processTime}
+response = {"response":"B2Found","confidence":"99.999" , "duration":"88.88"}
 
 jsonresponse = json.dumps(response)
 print(jsonresponse)
