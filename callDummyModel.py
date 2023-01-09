6c4a18a77aa3aaad7cad7de3bfc05d8ba05e0de3
Ian Brooks <ibrooks@cloudera.com>
2023-01-09 15:54:44 -0500

    Update callDummyModel.py

--- callDummyModel.py
+++ callDummyModel.py
@@ -2,10 +2,7 @@ from pprint import pprint
 import json
 import sys
 
-imagePath = str(sys.argv[1])
-threshold=0.95
-timeTheashold = 2.5
-
+#Dummy Response 
 response = {"response":"B2Found","confidence":"99.999" , "duration":"88.88"}
 
 jsonresponse = json.dumps(response)
