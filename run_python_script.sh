ef4710e43a756219c93fdd2c4e12954d98bc4cd1
Ian Brooks <ibrooks@cloudera.com>
2023-01-12 16:45:03 -0500

    update

---
+++ run_python_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Import Python Libriaes using pip
+pip install requests
+pip install pillow
+pip install numpy
+pip install image
+pip install Pillow-PIL
+
+python callTFB2Model.py $1
