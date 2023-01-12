1747983750b55691e40281128b1f118eff94816c
Ian Brooks <ibrooks@cloudera.com>
2023-01-12 16:01:01 -0500

    Update callDummyModel.py

--- callDummyModel.py
+++ callDummyModel.py
@@ -1,9 +1,11 @@
-from pprint import pprint
-import json
-import sys
-
-#Dummy Response 
-response = {"response":"B2Found","confidence":"99.999" , "duration":"88.88"}
-
-jsonresponse = json.dumps(response)
-print(jsonresponse)
+flowFileList = session.get(100)
+if not flowFileList.isEmpty():
+	for flowFile in flowFileList: 
+		newFlowFile = session.create(flowFile) 
+          
+	if (newFlowFile != None):
+		newFlowFile = session.putAttribute(newFlowFile, 'confidence', '99.99')
+        
+	if(errorOccurred){session.transfer(flowFile, REL_FAILURE)}
+	
+	else{session.transfer(flowFile, REL_SUCCESS)}	
