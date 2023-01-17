3943a7102390fd3a60a04b90f0ab7bd5875f92d4
Ian Brooks <ibrooks@cloudera.com>
2023-01-17 16:08:36 -0500

    update setup

--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-yum install conda
+#yum install conda
 
 #Active Python Environment
-conda create -n modelCall python=3.8
-conda activate modelCall
+#conda create -n modelCall python=3.8
+#conda activate modelCall
 
 #Import Python Libriaes using pip
-conda install requests
-conda install pillow
-conda install numpy
+pip3 install requests
+pip3 install pillow
+pip3 install numpy
 pip3 install image
 pip3 install Pillow-PIL
 
 pip3 install fastapi
 pip3 install "uvicorn[standard]"
 
-conda list
+#conda list
 
-conda deactivate
+#conda deactivate
 
 #Download Post Image Processor nar  - Thank You Tim Spann! 
 #wget https://github.com/tspannhw/nifi-postimage-processor/releases/download/1.0/nifi-postimage-nar-1.0.nar -O /usr/hdf/current/nifi/lib/nifi-postimage-nar-1.0.nar
