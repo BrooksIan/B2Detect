f189a72b476bd9352a6fc84a21c4c32ebd063aae
Ian Brooks <ibrooks@cloudera.com>
2023-01-30 12:36:31 -0500

    updates

--- appAPI/startRESTserver.sh
+++ appAPI/startRESTserver.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-uvicorn main:app --reload 
+cd appAPI/
+
+uvicorn --host 127.0.0.1 --port 4488 main:app --reload
--- testFASTserver.sh
+++ testFASTserver.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd appAPI/
 
 curl -X ‘GET’ \
   ‘http://127.0.0.1:8000/detectB2obj/777?q=https://farm66.staticflickr.com/65535/52582180138_fddffcbe97_n.jpg’ \
