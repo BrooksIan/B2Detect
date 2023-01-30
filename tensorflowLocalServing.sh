1ca0705388804479d8c55c7b569353fb1435cf5d
Ian Brooks <ibrooks@cloudera.com>
2023-01-30 12:37:10 -0500

    Update tensorflowLocalServing.sh

--- tensorflowLocalServing.sh
+++ tensorflowLocalServing.sh
@@ -4,6 +4,6 @@ docker pull tensorflow/serving
 
 #Adding the Version number on model target path is VERY important! 
 docker run -t --rm -p 8501:8501 \
- -v "/Users/ibrooks/Documents/GitHub/B2Detect/saved_model:/models/saved_model/1" \
+ -v "/saved_model:/models/saved_model/1" \
 -e MODEL_NAME=saved_model \
 tensorflow/serving &
